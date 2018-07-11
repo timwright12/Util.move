7e30539d47e44834721d9f6e805599c965231ee8
Jon Christensen <jon.christensen@10up.com>
2018-07-10 21:06:48 -0500

    Add matchmedia support

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -1,7 +1,10 @@
 import UtilityMove from '../../assets/js/UtilityMove.js';
 
 UtilityMove( {
-    'el'       : document.getElementById( 'one' ),
-    'appendTo' : document.getElementById( 'wrap' ),
-    'minWidth' : 800
+    el: document.getElementById( 'one' ),
+    appendTo: document.getElementById( 'wrap' ),
+    minWidth: 800,
+
+    useMatchMedia: true,
+    mediaQuery: '(min-width: 800px) and (min-height: 700px)',
 } );
